79a6969c216b7827b5e59f4d741f4cc1d1d3ab57
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2021-07-14 01:29:17 -0400

    removed all reviews (refresh)

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -8,28 +8,8 @@ const Reviews = () => {
       <Container>
         <div className="project-wrapper">
           <Title title="Reviews" />
-          {/* Google Reviews */}
-          <div
-            className="google-reviews"
-            data-romw-token="A6MgNg612OoQXL7t5FXCR1lHFltMyM2RQrrIrcXvFleEsnbNjQ"
-          />
-          {/* Twitter Feed */}
-          <a
-            className="twitter-timeline"
-            data-lang="en"
-            data-dnt="true"
-            href="https://twitter.com/KDKanwar?ref_src=twsrc%5Etfw"
-          >
-            Tweets by KDKanwar
-          </a>{' '}
         </div>
       </Container>
-
-      <script
-        src="https://reviewsonmywebsite.com/js/v2/embed.js?id=c338ae55b38e487f19ae"
-        type="text/javascript"
-      />
-      <script async src="https://platform.twitter.com/widgets.js" charset="utf-8" />
     </section>
   );
 };
