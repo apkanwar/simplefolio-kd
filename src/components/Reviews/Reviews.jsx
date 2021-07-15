7155ae194feea033b7ca7245bb9522f64418a65b
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2021-07-15 11:11:37 -0400

    Added google reviews to website

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import Title from '../Title/Title';
 
 const Reviews = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src = 'https://apps.elfsight.com/p/platform.js';
+    script.async = true;
+    script.defer = true;
+
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <section id="reviews">
       <Container>
         <div className="project-wrapper">
           <Title title="Reviews" />
+
+          <div className="elfsight-app-ce95dd2c-e35b-45da-85e7-57db36d0982b" />
+          <div className="hidden" />
         </div>
       </Container>
     </section>
